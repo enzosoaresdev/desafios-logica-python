75397319d151d5d5b23c40aaa1e4badd25567c45
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-07-09 10:21:01 -0300

    feat: Estrutura inicial dos exercícios de lógica

---
+++ soma.py
@@ -0,0 +1,8 @@
+#Soma de dois números em Python
+
+num1 = int(input("Digite o primeiro número para somar: "))
+num2 = int(input("Digite o segundo número para somar: "))
+
+soma = num1 + num2
+
+print(f"A soma dos número é: {soma}")
