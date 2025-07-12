65485a5dd684aee42fb235943d9ac149fbd95739
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-07-12 08:57:28 -0300

    feat: estrutura inícial do código qual o número maior

---
+++ numero-maior.py
@@ -0,0 +1,8 @@
+print("Digite dois número para saber qual é o maior:")
+num1 = int(input())
+num2 = int(input())
+
+if (num1>num2):
+    print(f"O número maior é: {num1}")
+else:
+    print(f"O número maior é: {num2}")
