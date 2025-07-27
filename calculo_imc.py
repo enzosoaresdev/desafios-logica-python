217ec8d817fc8576c72b1d5488946639a43f0f26
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-07-27 16:13:20 -0300

    feat: Correção do erro de sinal <

--- calculo_imc.py
+++ calculo_imc.py
@@ -1,23 +1,20 @@
-#Realiza o calculo de IMC de seu peso e altura.
-
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite sua altura: "))
 
 def calcular_imc(peso, altura):
     imc = peso / (altura*altura)
     return imc
- 
-resultado = calcular_imc(peso, altura)
-print(f"O seu IMC é: {resultado:.2f}")
 
-def classificar_imc(imc):
+resultado = calcular_imc(peso, altura)
+
+def classificacao_imc(imc):
     if imc < 18.5:
         return "Abaixo do peso."
     elif imc < 25:
-        return "Peso Normal."
+        return "Peso normal"
     elif imc < 30:
-        return "Sobrepeso."
+        return "Sobrepeso"
     else:
         return "Obesidade"
-    
-print(classificar_imc(resultado))
+
+print(classificacao_imc(resultado))
